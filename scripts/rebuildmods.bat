9f8bb3d3eeb9f84b0df86c34eb6f92d37b0486b6
seyhajin <seyhajin@gmail.com>
2021-01-06 19:14:19 +0100

    Update 'rebuildmods.bat'

--- scripts/rebuildmods.bat
+++ scripts/rebuildmods.bat
@@ -1,12 +1,14 @@
 
 echo off
 
+call common.bat
+
 echo.
 echo ***** Rebuilding modules *****
 echo.
 
-..\bin\windows\wake mods -clean -config=release -target=desktop
+%wake% mods -clean -config=release -target=desktop
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-..\bin\windows\wake mods -clean -config=debug -target=desktop
+%wake% mods -clean -config=debug -target=desktop
 if %errorlevel% neq 0 exit /b %errorlevel%
