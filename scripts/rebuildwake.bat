356720629aff42744a420d1829d09130069f90ff
seyhajin <seyhajin@gmail.com>
2021-01-06 19:00:44 +0100

    Fix 'rebuildwake.bat'

--- scripts/rebuildwake.bat
+++ scripts/rebuildwake.bat
@@ -10,7 +10,7 @@ echo.
 %wake% mods -clean -config=release wonkey libc miniz stb-image stb-image-write stb-vorbis zlib std
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%wake% app -clean -apptype=console -config=release -product=scripts/wake.products/wake_windows.exe ../src/wake/wake.wonkey2
+%wake% app -clean -apptype=console -config=release -product=scripts/wake.products/wake_windows.exe ../src/wake/wake.wx
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 copy wake.products\wake_windows.exe %wake%
