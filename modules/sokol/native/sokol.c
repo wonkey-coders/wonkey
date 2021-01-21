51289a0b2b4c42275e149348fa3e9915f1d77c0c
seyhajin <seyhajin@gmail.com>
2021-01-21 23:00:01 +0100

    Updated 'sokol' module api

---
+++ modules/sokol/native/sokol.c
@@ -0,0 +1,26 @@
+#define SOKOL_IMPL
+#define SOKOL_NO_ENTRY // define this if sokol_app.h shouldn't "hijack" the main() function
+#if defined(_WIN32)
+#define SOKOL_LOG(s) OutputDebugStringA(s)
+#endif
+/* this is only needed for the debug-inspection headers */
+#define SOKOL_TRACE_HOOKS
+/* 
+    sokol 3D-API defines are provided by build options 
+
+    #define SOKOL_GLCORE33
+    #define SOKOL_GLES2
+    #define SOKOL_GLES3
+    #define SOKOL_D3D11
+    #define SOKOL_METAL
+    #define SOKOL_WGPU
+
+*/
+#define SOKOL_GLCORE33
+
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+//#include "sokol_time.h"
+//#include "sokol_audio.h"
+//#include "sokol_fetch.h"
+#include "sokol_glue.h"
