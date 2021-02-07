952778b7d667c001539e770734fba1942b484f9c
D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>
2021-02-07 13:21:55 +0100

    Removed -clean from building mods.
    Signed-off-by: D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>

--- scripts/rebuildwake.sh
+++ scripts/rebuildwake.sh
@@ -1,11 +1,11 @@
-
+﻿
 . ./common.sh
 
 echo ""
 echo "***** Rebuilding wake *****"
 echo ""
 
-$wake mods -clean -config=release wonkey libc miniz stb-image stb-image-write stb-vorbis zlib std
+$wake mods -config=release wonkey libc miniz stb-image stb-image-write stb-vorbis zlib std
 
 $wake app -clean -apptype=console -config=release -product=scripts/wake.products/wake_$host ../src/wake/wake.wx
 
