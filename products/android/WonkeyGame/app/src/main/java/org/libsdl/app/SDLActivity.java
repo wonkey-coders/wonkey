97bc0b576b92fb7a62042eac498fdaf75ae42d3d
D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>
2021-06-21 03:14:29 +0200

    Fixed dynamic lib loading
    Signed-off-by: D-a-n-i-l-o <D-a-n-i-l-o@users.noreply.github.com>

--- products/android/WonkeyGame/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ products/android/WonkeyGame/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -163,13 +163,14 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
      */
     protected String[] getLibraries() {
         return new String[] {
-            //"hidapi",
-            "SDL2",
+            //!\\ "hidapi",
+            //!\\ "SDL2",
             // "SDL2_image",
             // "SDL2_mixer",
             // "SDL2_net",
             // "SDL2_ttf",
-            "main"
+            "openal", //!\\
+            "wx_main" //!\\
         };
     }
 
