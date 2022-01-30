e3a2688f4479f817a279ee79dc0bd1ff58510595
seyhajin <seyhajin@gmail.com>
2022-01-30 12:59:31 +0100

    Fixed 'launcher' resource object linking (32bit version)

--- src/launcher/update_rc.bat
+++ src/launcher/update_rc.bat
@@ -1,6 +1,5 @@
+windres.exe --target=pe-i386 resource.rc resource.o
 
-windres.exe resource.rc resource.o
-
-windres.exe resource.rc resource_x64.o
+windres.exe --target=pe-x86-64 resource.rc resource_x64.o
 
 pause
